1b76f652dda59c384c97925d7cba19b42b0aca54
mac <mac@macs-MacBook-Pro-2.local>
2024-04-16 16:23:09 +0500

    Add existing project files to Git

---
+++ ebs.tf
@@ -0,0 +1,63 @@
+
+
+resource "aws_elastic_beanstalk_application" "tftest" {
+  name        = "tf-test-name"
+  description = "tf-test-desc"
+}
+
+data "aws_elastic_beanstalk_solution_stack" "latest_docker" {
+  most_recent = true
+  name_regex  = ".*Amazon Linux 2.*running Docker.*"
+}
+
+
+resource "aws_elastic_beanstalk_environment" "tfenvtest" {
+  name                = "tf-test-name"
+  application         = aws_elastic_beanstalk_application.tftest.name
+  solution_stack_name = data.aws_elastic_beanstalk_solution_stack.latest_docker.name 
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "InstanceType"
+    value     = var.ec2-instance-type
+  }
+
+   setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = "aws-elasticbeanstalk-ec2-role"
+  }
+
+    setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "MinSize"
+    value     = var.ec2-instance-minSize
+  }
+
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "MaxSize"
+    value     = var.ec2-instance-maxSize
+  }
+
+    setting {
+    namespace = "aws:elasticbeanstalk:cloudwatch:logs"
+    name      = "StreamLogs"
+    value     = "true"
+  }
+
+  
+
+#   setting {
+#     namespace = "aws:ec2:vpc"
+#     name      = "VPCId"
+#     value     = module.vpc.vpc_id
+#   }
+# setting {
+#     namespace = "aws:ec2:vpc"
+#     name      = "Subnets"
+#     # value     = join(",", data.aws_subnet_ids.public_subnets.ids) 
+#      value     =  module.vpc.public_subnets
+#   }
+
+}
---
+++ variables.tf
@@ -0,0 +1,41 @@
+variable "region" {
+  type = string
+  default = "us-west-1"
+}
+
+variable "ec2-instance-type" {
+  type = string
+  default = "t2.micro"
+}
+
+variable "ec2-instance-minSize" {
+  type = number
+  default = 1
+}
+
+variable "ec2-instance-maxSize" {
+  type = number
+  default = 1
+}
+
+variable "postgres_username" {
+ type = string
+default = "neochargedev"
+}
+
+variable "postgres_password" {
+ description = "RDS root user password"
+   default = 12345678
+}
+
+variable "project_name" {
+ type = string
+  default = "neocharge"
+}
+
+variable "public_subnets" {
+  type    = list(string)
+  default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+
