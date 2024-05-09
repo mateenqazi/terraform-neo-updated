c5dd69bdacd00c5ada81a23b19511aac0400a50c
Mateen <mateenqazi125@gmail.com>
2024-05-09 13:48:30 +0500

    immutable development

--- modules/ebs/main.tf
+++ modules/ebs/main.tf
@@ -72,67 +72,67 @@ data "aws_elastic_beanstalk_solution_stack" "latest_docker" {
 # }
 
 # Blue Environment
-resource "aws_elastic_beanstalk_environment" "blue" {
-  name                = "neocharge-blue-${var.environment}"
-  application         = aws_elastic_beanstalk_application.ebs_neocharge_application.name
-  solution_stack_name = data.aws_elastic_beanstalk_solution_stack.latest_docker.name
-  # Additional settings...
+# resource "aws_elastic_beanstalk_environment" "blue" {
+#   name                = "neocharge-blue-${var.environment}"
+#   application         = aws_elastic_beanstalk_application.ebs_neocharge_application.name
+#   solution_stack_name = data.aws_elastic_beanstalk_solution_stack.latest_docker.name
+#   # Additional settings...
 
-  setting {
-    namespace = "aws:ec2:vpc"
-    name      = "VPCId"
-    value     = var.vpc_id
-  }
+#   setting {
+#     namespace = "aws:ec2:vpc"
+#     name      = "VPCId"
+#     value     = var.vpc_id
+#   }
 
-  setting {
-    namespace = "aws:ec2:vpc"
-    name      = "Subnets"
-    value     = join(",", var.subnets)
-  }
+#   setting {
+#     namespace = "aws:ec2:vpc"
+#     name      = "Subnets"
+#     value     = join(",", var.subnets)
+#   }
 
-  setting {
-    namespace = "aws:ec2:vpc"
-    name      = "AssociatePublicIpAddress"
-    value     = "true"
-  }
+#   setting {
+#     namespace = "aws:ec2:vpc"
+#     name      = "AssociatePublicIpAddress"
+#     value     = "true"
+#   }
 
 
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "SecurityGroups"
-    value     = join(",", var.security_groups)
-  }
+#   setting {
+#     namespace = "aws:autoscaling:launchconfiguration"
+#     name      = "SecurityGroups"
+#     value     = join(",", var.security_groups)
+#   }
 
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "InstanceType"
-    value     = var.ec2-instance-type
-  }
+#   setting {
+#     namespace = "aws:autoscaling:launchconfiguration"
+#     name      = "InstanceType"
+#     value     = var.ec2-instance-type
+#   }
 
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "IamInstanceProfile"
-    value     = "aws-elasticbeanstalk-ec2-role"
-  }
+#   setting {
+#     namespace = "aws:autoscaling:launchconfiguration"
+#     name      = "IamInstanceProfile"
+#     value     = "aws-elasticbeanstalk-ec2-role"
+#   }
 
-  setting {
-    namespace = "aws:autoscaling:asg"
-    name      = "MinSize"
-    value     = var.ec2-instance-min-size
-  }
+#   setting {
+#     namespace = "aws:autoscaling:asg"
+#     name      = "MinSize"
+#     value     = var.ec2-instance-min-size
+#   }
 
-  setting {
-    namespace = "aws:autoscaling:asg"
-    name      = "MaxSize"
-    value     = var.ec2-instance-max-size
-  }
+#   setting {
+#     namespace = "aws:autoscaling:asg"
+#     name      = "MaxSize"
+#     value     = var.ec2-instance-max-size
+#   }
 
-  setting {
-    namespace = "aws:elasticbeanstalk:cloudwatch:logs"
-    name      = "StreamLogs"
-    value     = "true"
-  }
-}
+#   setting {
+#     namespace = "aws:elasticbeanstalk:cloudwatch:logs"
+#     name      = "StreamLogs"
+#     value     = "true"
+#   }
+# }
 
 # Green Environment
 resource "aws_elastic_beanstalk_environment" "green" {
@@ -194,5 +194,11 @@ resource "aws_elastic_beanstalk_environment" "green" {
     namespace = "aws:elasticbeanstalk:cloudwatch:logs"
     name      = "StreamLogs"
     value     = "true"
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:command"
+    name      = "DeploymentPolicy"
+    value     = "Immutable"
   }
 }
